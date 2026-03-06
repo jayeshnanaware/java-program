bf358153ff7c3ad098f400ab1090f6f18576f73c
jayeshnanaware <148766078+jayeshnanaware@users.noreply.github.com>
2026-03-06 21:37:42 +0530

    Add files via upload

---
+++ day2p1.java
@@ -0,0 +1,11 @@
+
+class Demo{
+	public static void main(String[] args){
+		int x=10;
+		int y=20;
+		Demo Obj=new Demo();
+		System.out.println("In main Method");
+		System.out.println(x+y);
+	}
+}
+
