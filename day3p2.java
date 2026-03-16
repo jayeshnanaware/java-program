e447245f6030adec98fe118a57cc6a430dea44a8
jayeshnanaware <148766078+jayeshnanaware@users.noreply.github.com>
2026-03-16 22:35:44 +0530

    Add files via upload

---
+++ day3p2.java
@@ -0,0 +1,7 @@
+class Demo{
+	public static void main(String[] args){
+		short a=144;
+		System.out.println(a);
+		//short is another datatype of integer  having range of values -32768 to 32767
+	}
+} 
