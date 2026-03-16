1a31bfb31ef1aa4a41bcac4800191d9281e6a258
jayeshnanaware <148766078+jayeshnanaware@users.noreply.github.com>
2026-03-16 22:37:48 +0530

    Add files via upload

---
+++ day3p8.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String[] args){
+		var x=10;
+		var y=20.5;
+		var z='A';
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
+
