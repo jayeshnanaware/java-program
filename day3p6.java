00e2645b663f63fb2ef1982921277ebe1fa52ce3
jayeshnanaware <148766078+jayeshnanaware@users.noreply.github.com>
2026-03-16 22:37:07 +0530

    Add files via upload

---
+++ day3p6.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String[] args){
+		char ch='A';
+		System.out.println(ch);
+		
+		String str="\u091C\u092F\u0947\u0936 \u0928\u0928\u093E\u0935\u0930\u0947";
+		//this prints name in marathi language 
+		//this shows unicode property of java
+		System.out.println(str);
+	}
+}
