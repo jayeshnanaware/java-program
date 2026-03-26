814a1984ec7a85dfa7479353ab2298217cb8bc06
jayeshnanaware <148766078+jayeshnanaware@users.noreply.github.com>
2026-03-26 23:39:37 +0530

    Add files via upload

---
+++ program16.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo{
+        public static void main(String[] args){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter number:");
+                int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+			int num=1;
+			for(int j=1;j<=n;j++){
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+	}
+}
